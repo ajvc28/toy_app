82ae1378259e80f17a84562cf7c1af166e29d561
adrian castillo <38401246+ajvc28@users.noreply.github.com>
2022-09-18 13:42:02 -1000

    Add files via upload

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+
+Bundler.require(*Rails.groups)
+
+module ToyApp
+  class Application < Rails::Application
+
+    config.load_defaults 6.0
+
+   
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,19 @@
+
+
+ActiveRecord::Schema.define(version: 2019_10_31_173702) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
